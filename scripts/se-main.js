bfcae3c01a54c8681f51f2f3e53bbe191cb27b4f
Swifty Wu <swifty@erniewest-lm.tpcity.corp.yahoo.com>
2015-01-16 13:42:36 +0800

    js

---
+++ scripts/se-main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var options = {
+        autoPlay: true
+        autoPlayDelay: 5000
+    }
+    var sequence = $("#sequence").sequence(options).data("sequence");
+    sequence.beforeCurrentFrameAnimatesOut = function(){
+        //add code to execute here, such as:
+        console.log('animates-out',sequence);
+        // alert("Do something before the CURRENT frame animates out");
+    };
+
+    sequence.beforeNextFrameAnimatesIn = function(){
+        //add code to execute here, such as:
+        // $('.my-image').css('hight','100px');
+        console.log('animates-in',sequence);
+
+        // alert("Do something before the NEXT frame animates in");
+    };
+});
