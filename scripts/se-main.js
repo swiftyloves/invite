607d2be91c97ad3c7130fdc5ee46583ba8815fb4
Swifty Wu <swifty@erniewest-lm.(none)>
2015-01-17 09:46:14 +0800

    det final not work

--- scripts/se-main.js
+++ scripts/se-main.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+
     console.log('navigator.userAgen: ',navigator.userAgen);
     if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $('#sequence').css('display','block');
+        
         var options = {
             autoPlay: true,
-            autoPlayDelay: 3000
+            autoPlayDelay: 1500,
+            preventDelayWhenReversingAnimations: true,
+            preloader: true,
+            KeyEvents: {
+                right: "next"
+            }
         }
-        // var sequence = $("#sequence").sequence(options).data("sequence");
-        var sequence = $("#sequence").sequence().data("sequence");
+        var sequence = $("#sequence").sequence(options).data("sequence");
+        console.log(sequence.next);
+        sequence.next = function (){
+            var a=this;id=(a.currentFrameID!==a.numberOfFrames)?(a.currentFrameID+1):(a.currentFrameID),(a.active===!1||void 0===a.active?a.goTo(id,1):a.goTo(id,1,!0))
+        }
         $('.device').css('display','none');
     }else{
         $('.device').css('display','block');
