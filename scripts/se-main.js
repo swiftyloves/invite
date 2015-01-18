b451492fa0059785c2b9be4c1146bf6402755d4d
Swifty Wu <swifty@erniewest-lm.(none)>
2015-01-18 18:47:11 +0800

    mobile version

--- scripts/se-main.js
+++ scripts/se-main.js
@@ -1,34 +1,28 @@
 $(document).ready(function(){
 
     console.log('navigator.userAgen: ',navigator.userAgen);
-    if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('#sequence').css('display','block');
-        
-        var options = {
-            // autoPlay: true,
-            autoPlayDelay: 1500,
-            preventDelayWhenReversingAnimations: true,
-            preloader: true,
-            KeyEvents: {
-                right: "next"
-            }
+    $('#sequence').css('display','block');
+    var options = {
+        // autoPlay: true,
+        autoPlayDelay: 1500,
+        preventDelayWhenReversingAnimations: true,
+        preloader: true,
+        KeyEvents: {
+            right: "next"
         }
-        var sequence = $("#sequence").sequence(options).data("sequence");
-        console.log(sequence.prev);
-        sequence.next = function (){
-            var a=this;id=(a.currentFrameID!==a.numberOfFrames)?(a.currentFrameID+1):(a.currentFrameID),(a.active===!1||void 0===a.active?a.goTo(id,1):a.goTo(id,1,!0))
-        }
-        sequence.prev = function (){
-            var a=this;id=(1===a.currentFrameID)?1:a.currentFrameID-1,a.active===!1||void 0===a.active?a.goTo(id,-1):a.goTo(id,-1,!0)
-        }
-
-        $('.device').css('display','none');
-    }else{
+    }
+    var sequence = $("#sequence").sequence(options).data("sequence");
+    console.log(sequence.prev);
+    sequence.next = function (){
+        var a=this;id=(a.currentFrameID!==a.numberOfFrames)?(a.currentFrameID+1):(a.currentFrameID),(a.active===!1||void 0===a.active?a.goTo(id,1):a.goTo(id,1,!0))
+    }
+    sequence.prev = function (){
+        var a=this;id=(1===a.currentFrameID)?1:a.currentFrameID-1,a.active===!1||void 0===a.active?a.goTo(id,-1):a.goTo(id,-1,!0)
+    }
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $('.cake').addClass('cake-mobile');
         $('.table').addClass('table-mobile');
         $('.beckie').addClass('beckie-mobile');
         $('.laugh').addClass('laugh-mobile');
-        $('.device').css('display','block');
-        $('#sequence').css('display','none');
     }
 });
