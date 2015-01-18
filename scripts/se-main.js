efcf262c2236d0a9c219989761ce300cdcfbd5ca
Swifty Wu <swifty@erniewest-lm.(none)>
2015-01-18 18:57:07 +0800

    modify location

--- scripts/se-main.js
+++ scripts/se-main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-    console.log('navigator.userAgen: ',navigator.userAgen);
     $('#sequence').css('display','block');
     var options = {
         // autoPlay: true,
@@ -12,17 +11,10 @@ $(document).ready(function(){
         }
     }
     var sequence = $("#sequence").sequence(options).data("sequence");
-    console.log(sequence.prev);
     sequence.next = function (){
         var a=this;id=(a.currentFrameID!==a.numberOfFrames)?(a.currentFrameID+1):(a.currentFrameID),(a.active===!1||void 0===a.active?a.goTo(id,1):a.goTo(id,1,!0))
     }
     sequence.prev = function (){
         var a=this;id=(1===a.currentFrameID)?1:a.currentFrameID-1,a.active===!1||void 0===a.active?a.goTo(id,-1):a.goTo(id,-1,!0)
-    }
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('.cake').addClass('cake-mobile');
-        $('.table').addClass('table-mobile');
-        $('.beckie').addClass('beckie-mobile');
-        $('.laugh').addClass('laugh-mobile');
     }
 });
