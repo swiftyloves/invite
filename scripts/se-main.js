2067cd7fea7af243fc8cfc06871827dd37f06b21
Swifty Wu <swifty@erniewest-lm.(none)>
2015-01-18 18:45:48 +0800

    add mobile version

--- scripts/se-main.js
+++ scripts/se-main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $('#sequence').css('display','block');
         
         var options = {
-            autoPlay: true,
+            // autoPlay: true,
             autoPlayDelay: 1500,
             preventDelayWhenReversingAnimations: true,
             preloader: true,
@@ -14,12 +14,20 @@ $(document).ready(function(){
             }
         }
         var sequence = $("#sequence").sequence(options).data("sequence");
-        console.log(sequence.next);
+        console.log(sequence.prev);
         sequence.next = function (){
             var a=this;id=(a.currentFrameID!==a.numberOfFrames)?(a.currentFrameID+1):(a.currentFrameID),(a.active===!1||void 0===a.active?a.goTo(id,1):a.goTo(id,1,!0))
         }
+        sequence.prev = function (){
+            var a=this;id=(1===a.currentFrameID)?1:a.currentFrameID-1,a.active===!1||void 0===a.active?a.goTo(id,-1):a.goTo(id,-1,!0)
+        }
+
         $('.device').css('display','none');
     }else{
+        $('.cake').addClass('cake-mobile');
+        $('.table').addClass('table-mobile');
+        $('.beckie').addClass('beckie-mobile');
+        $('.laugh').addClass('laugh-mobile');
         $('.device').css('display','block');
         $('#sequence').css('display','none');
     }
